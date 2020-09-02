acfee3dc4c8c541cf2f262b48352b69bdeaedc34
atBeatty <adam.t.beatt@gmail.com>
2020-09-01 17:10:52 -0700

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
 class BankAccount
+    attr_accessor :balance, :status, :transfer
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def deposit(amount)
+        @balance +=amount
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @balance > 0 && @status == "open"
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
+
+
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,78 @@
+require 'pry'
+
 class Transfer
   # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+
+  end
+
+  def valid?
+ 
+    
+
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+
+    # transaction_members = BankAccount.all.select{|named_account| named_account.name == @sender.name || named_account.name == @receiver.name}
+    # invalid_part = transaction_members.detect do |member|
+    #   member.valid?
+    #   # member.status == "closed" || member.balance < 1000
+    # end
+    # # binding.pry
+
+    # if invalid_part
+    #   true
+    # else
+    #   false
+    # end
+
+  end
+
+  def execute_transaction
+   
+    if @sender.valid? && @receiver.valid? && @status == "pending" && @sender.balance > @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+
+    end
+
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+      "It has been reversed"
+    end
+
+  end
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
 end
