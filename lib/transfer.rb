848cf203c5ebb4c2dbeee72c2ada182a656b4786
mollymoog <mmkover@gmail.com>
2020-09-03 19:13:17 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+  sender_good = BankAccount.new(sender).valid?
+  receiver_good = BankAccount.new(receiver).valid?
+
+    sender_good
+    receiver_good
+
+  # if sender_good == receiver_good && receiver_good == true
+  #   true
+  # end
+  # # binding.pry
+end
+
+
 end
